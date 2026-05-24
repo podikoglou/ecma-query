21b1bc7f4233eb2cb8e03413cd0a1f239013873c
alex <alex.podikoglou@gmail.com>
2026-05-24 13:21:38 +0300

    feat: add index fields to Spec, ClauseNode, and new types GrammarProd/XRef

--- internal/spec/types.go
+++ internal/spec/types.go
@@ -8,6 +8,15 @@ type Spec struct {
 	Root  *ClauseNode
 	Nodes []*ClauseNode
 	ByID  map[string]*ClauseNode
+
+	SectionNums  map[string]*ClauseNode
+	OpsByName    map[string][]*ClauseNode
+	Outgoing     map[string]map[string]struct{}
+	Incoming     map[string]map[string]struct{}
+	XRefs        []XRef
+	TextIndex    map[string]map[string]int
+	ClauseTokens map[string]int
+	Grammars     map[string][]GrammarProd
 }
 
 // ClauseNode represents a section of the spec (clause, annex, or intro).
@@ -15,9 +24,25 @@ type ClauseNode struct {
 	ID       string
 	Kind     string
 	Type     string
+	Section  string
+	Name     string
 	Depth    int
 	HTMLNode *html.Node
 	H1Text   string
 	Parent   *ClauseNode
 	Children []*ClauseNode
+}
+
+// GrammarProd represents a grammar production found in the spec.
+type GrammarProd struct {
+	Name    string
+	Section string
+	HTML    string
+}
+
+// XRef represents a cross-reference from one clause to another.
+type XRef struct {
+	Source  string
+	Target  string
+	Context string
 }
